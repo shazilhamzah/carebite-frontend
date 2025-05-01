a825e4e0e5ed6d6ba02d7fe7bcd558149de18937
darklariat <msaad.95a@gmail.com>
2025-05-01 12:42:25 +0500

    resoled merge conflicts 2

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 "use client"
 
 import type React from "react"
-=======
-"use client";
->>>>>>> main
 
 import {
   SidebarGroup,
@@ -12,21 +8,13 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-<<<<<<< HEAD
 } from "@/components/ui/sidebar"
 import { useGlobalState } from "@/lib/globalStates"
-=======
-} from "@/components/ui/sidebar";
-import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
-import { useGlobalState } from "@/lib/globalStates";
->>>>>>> main
 
 export function NavMain({
   items,
 }: {
   items: {
-<<<<<<< HEAD
     title: string
     url: string
     icon?: React.ComponentType<any>
@@ -39,18 +27,6 @@ export function NavMain({
     console.log(userTab)
   }
 
-=======
-    title: string;
-    url: string;
-    icon?: React.ComponentType<any>;
-  }[];
-}) {
-  const { setUserTab,userTab } = useGlobalState();
-  const handleClick = (title: string) => {
-    setUserTab(title);
-    console.log(userTab);
-  };
->>>>>>> main
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
@@ -60,14 +36,7 @@ export function NavMain({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-<<<<<<< HEAD
               <SidebarMenuButton onClick={() => handleClick(item.title)} tooltip={item.title}>
-=======
-              <SidebarMenuButton
-                onClick={() => handleClick(item.title)}
-                tooltip={item.title}
-              >
->>>>>>> main
                 {item.icon && <item.icon />}
                 <span>{item.title}</span>
               </SidebarMenuButton>
@@ -76,9 +45,5 @@ export function NavMain({
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
-<<<<<<< HEAD
   )
-=======
-  );
->>>>>>> main
 }
--- src/lib/nav-data.ts
+++ src/lib/nav-data.ts
@@ -22,56 +22,27 @@ import {
   IconVocabulary,
   IconReportMedical,
   IconCalendarWeek,
-<<<<<<< HEAD
 } from "@tabler/icons-react";
 import { IconShare } from "@tabler/icons-react"
 import { ChefHat, Coins, Hospital, PersonStanding } from "lucide-react";
 
 export type UserRole = "worker" | "gmHosp" | "gmCord" | "GS" | "Donors" | "ADM" | "Supervisor" | "gm" | "etc.";
-=======
-  IconCalendar,
-} from "@tabler/icons-react";
-import { ChefHat, Coins, Hospital, PersonStanding } from "lucide-react";
-
-type UserRole =
-  | "worker"
-  | "gmHosp"
-  | "gmCord"
-  | "GS"
-  | "Donors"
-  | "ADM"
-  | "Supervisor"
-  | "gm"
-  | "etc.";
->>>>>>> main
 
 export function getNavData(role: UserRole) {
   switch (role) {
     case "Donors":
-<<<<<<< HEAD
       return{
         navMain: [
           { title: "Personal Information", url: "#", icon: PersonStanding},
           { title: "Funds Status", url: "#", icon: IconAdjustmentsDollar },
           { title: "Requests", url: "#", icon: IconUserQuestion},
-=======
-      return {
-        navMain: [
-          { title: "Personal Information", url: "#", icon: PersonStanding },
-          { title: "Funds Status", url: "#", icon: IconAdjustmentsDollar },
-          { title: "Requests", url: "#", icon: IconUserQuestion },
->>>>>>> main
           { title: "Hospitals Information", url: "#", icon: Hospital },
           { title: "Food Status", url: "#", icon: IconToolsKitchen3 },
           { title: "Announcements", url: "#", icon: IconSpeakerphone },
         ],
-<<<<<<< HEAD
         documents: [
           { name: "Hospital Policy", url: "/docs/hospital-policy", icon: IconFolder },
           { name: "Donation Guidelines", url: "/docs/donation", icon: IconShare },],
-=======
-        documents: [],
->>>>>>> main
         navSecondary: [],
       };
 
@@ -79,20 +50,12 @@ export function getNavData(role: UserRole) {
       return {
         navMain: [
           { title: "Personal Information", url: "#", icon: PersonStanding },
-<<<<<<< HEAD
           { title: "Reviews", url: "#", icon: IconDevicesCheck},
-=======
-          { title: "Reviews", url: "#", icon: IconDevicesCheck },
->>>>>>> main
           { title: "Requests", url: "#", icon: IconUserQuestion },
           { title: "Donations", url: "#", icon: IconMoneybag },
           { title: "Hospitals Information", url: "#", icon: Hospital },
           { title: "Funds Status", url: "#", icon: IconAdjustmentsDollar },
-<<<<<<< HEAD
           { title: "Attendance", url: "#", icon: IconCalendarWeek},
-=======
-          { title: "Attendance", url: "#", icon: IconCalendarWeek },
->>>>>>> main
           { title: "Salary", url: "#", icon: Coins },
           { title: "Announcements", url: "#", icon: IconSpeakerphone },
         ],
@@ -100,28 +63,16 @@ export function getNavData(role: UserRole) {
         navSecondary: [],
       };
 
-<<<<<<< HEAD
      case "gmCord":
-=======
-    case "gmCord":
->>>>>>> main
       return {
         navMain: [
           { title: "Personal Information", url: "#", icon: PersonStanding },
           { title: "Reviews", url: "#", icon: IconDevicesCheck },
-<<<<<<< HEAD
           { title: "Requests", url: "#", icon: IconUserQuestion},
           { title: "Hospitals Information", url: "#", icon:  Hospital },
           { title: "Funds Status", url: "#", icon: IconAdjustmentsDollar },
           { title: "Attendance", url: "#", icon: IconCalendarWeek},
           { title: "Salary", url: "#", icon: Coins  },
-=======
-          { title: "Requests", url: "#", icon: IconUserQuestion },
-          { title: "Hospitals Information", url: "#", icon: Hospital },
-          { title: "Funds Status", url: "#", icon: IconAdjustmentsDollar },
-          { title: "Attendance", url: "#", icon: IconCalendarWeek },
-          { title: "Salary", url: "#", icon: Coins },
->>>>>>> main
           { title: "Announcements", url: "#", icon: IconSpeakerphone },
         ],
         documents: [],
@@ -132,21 +83,12 @@ export function getNavData(role: UserRole) {
       return {
         navMain: [
           { title: "Personal Information", url: "#", icon: PersonStanding },
-<<<<<<< HEAD
           { title: "Hospital Information", url: "#", icon:  Hospital  },
           { title: "Reviews", url: "#", icon: IconDevicesCheck },
           { title: "Menu", url: "#", icon: IconVocabulary },
           { title: "Food status", url: "#", icon: IconBowl },
           { title: "Attendance", url: "#", icon:IconCalendarWeek},
           { title: "Salary", url: "#", icon: Coins  },
-=======
-          { title: "Hospital Information", url: "#", icon: Hospital },
-          { title: "Reviews", url: "#", icon: IconDevicesCheck },
-          { title: "Menu", url: "#", icon: IconVocabulary },
-          { title: "Food status", url: "#", icon: IconBowl },
-          { title: "Attendance", url: "#", icon: IconCalendarWeek },
-          { title: "Salary", url: "#", icon: Coins },
->>>>>>> main
           { title: "Announcements", url: "#", icon: IconSpeakerphone },
         ],
         documents: [],
@@ -161,13 +103,8 @@ export function getNavData(role: UserRole) {
           { title: "Requests", url: "#", icon: IconUserQuestion },
           { title: "Hospital Salaries", url: "#", icon: IconReportMedical },
           { title: "Funds Status", url: "#", icon: IconAdjustmentsDollar },
-<<<<<<< HEAD
           { title: "Food Stock", url: "#", icon: IconFridge},
           { title: "Attendance", url: "#", icon: IconCalendarWeek},
-=======
-          { title: "Food Stock", url: "#", icon: IconFridge },
-          { title: "Attendance", url: "#", icon: IconCalendarWeek },
->>>>>>> main
           { title: "Salary", url: "#", icon: Coins },
           { title: "Announcements", url: "#", icon: IconSpeakerphone },
         ],
@@ -179,27 +116,16 @@ export function getNavData(role: UserRole) {
       return {
         navMain: [
           { title: "Personal Information", url: "#", icon: PersonStanding },
-<<<<<<< HEAD
           { title: "Hospital Information", url: "#", icon:  Hospital  },
           { title: "Reviews", url: "#", icon: IconDevicesCheck },
           { title: "Requests", url: "#", icon: IconUserQuestion},
           { title: "Attendance", url: "#", icon: IconCalendarWeek},
           { title: "Salary", url: "#", icon: Coins  },
           { title: "Announcements", url: "#", icon: IconSpeakerphone},
-=======
-          { title: "Hospital Information", url: "#", icon: Hospital },
-          // { title: "Reviews", url: "#", icon: IconDevicesCheck },
-          { title: "Requests", url: "#", icon: IconUserQuestion },
-          { title: "Attendance", url: "#", icon: IconCalendarWeek },
-          { title: "Workers Attendance", url: "#", icon: IconCalendar },
-          { title: "Salary", url: "#", icon: Coins },
-          { title: "Announcements", url: "#", icon: IconSpeakerphone },
->>>>>>> main
         ],
         documents: [],
         navSecondary: [],
       };
-<<<<<<< HEAD
       
     case "worker":
       return {
@@ -207,19 +133,6 @@ export function getNavData(role: UserRole) {
           { title: "Personal Information", url: "/dashboard/personal-information", icon: PersonStanding },
           { title: "Attendance", url: "#", icon: IconCalendarWeek},
           { title: "Salary", url: "#", icon: Coins  },
-=======
-
-    case "worker":
-      return {
-        navMain: [
-          {
-            title: "Personal Information",
-            url: "/dashboard/personal-information",
-            icon: PersonStanding,
-          },
-          { title: "Attendance", url: "#", icon: IconCalendarWeek },
-          { title: "Salary", url: "#", icon: Coins },
->>>>>>> main
           { title: "Announcements", url: "#", icon: IconSpeakerphone },
         ],
         documents: [],
