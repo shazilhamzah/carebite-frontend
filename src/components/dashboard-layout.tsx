474ddeedca66cc64663bd52c90ac73a3330f3877
Shazil Hamzah <shazil.hamzah@outlook.com>
2025-05-08 19:49:19 +0500

    dashboard-layout bug fix

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -3,12 +3,13 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { UserRole } from "@/lib/nav-data";
 
 export default function DashboardLayout({
   userRole,
   children,
 }: {
-  userRole: string;
+  userRole: UserRole;
   children: React.ReactNode;
 }) {
   return (
@@ -20,7 +21,7 @@ export default function DashboardLayout({
         } as React.CSSProperties
       }
     >
-      <AppSidebar variant="inset" userRole={userRole} />
+      <AppSidebar variant="inset" userRole={userRole as any} />
       <SidebarInset>
         <SiteHeader />
         {children}
