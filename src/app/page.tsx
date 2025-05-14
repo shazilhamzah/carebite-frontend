874a972e4babaad2356af94f8044235003d74db6
Shazil Hamzah <shazil.hamzah@outlook.com>
2025-05-15 04:05:03 +0500

    home page directs to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
 "use client";
 import "@/app/globals.css";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  return (
-    <>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        onClick={() => router.push("/login")}
-      >
-        Go to Login
-      </button>
-      <br></br>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        onClick={() => router.push("/dashboard")}
-      >
-        Go to Dashboard
-      </button>
-    </>
-  );
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
+  return null; // Optionally, show a loader here instead of null
 }
